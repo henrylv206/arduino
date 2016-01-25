b211e5ef4973561a667b157123f8385b2becf22c
Honglei.Lv <lvhonglei@syswin.com>
2016-01-25 14:18:37 +0800

    add test code

--- helloworld/helloworld.ino
+++ helloworld/helloworld.ino
@@ -1,9 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+const int buttonPin = 3;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (digitalRead(buttonPin) == HIGH) {
+    Serial.write('H');
+  } else {
+    Serial.write('L');
+  }
 
+  delay(1000);
 }
+
+void test() {
+  Serial.read();
+}
+
